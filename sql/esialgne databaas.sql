6d5651aa728bd0e7f336eecfa65b00714f7ec353
vellov <vello.vaherpuu@hotmail.com>
2015-02-17 11:07:09 +0200

    Algne databaas testiks

---
+++ sql/esialgne databaas.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `Candidates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO `Candidates` VALUES (1,'Kandidaat 1'),(2,'Kandidaat2');
