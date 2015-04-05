c16a0da29dfc178a35cc534632347a118cf26621
vellovaherpuu <vello@bitweb.ee>
2015-04-05 20:20:26 +0300

    active class fix, removed tulemused

--- web/app/scripts/getCandidates.php
+++ web/app/scripts/getCandidates.php
@@ -10,7 +10,7 @@ include 'conf.php';
 //Baasist kandidaadid, kus election_id=parameeter $http päringust
 $params=$_GET;
 $electionId=$params["electionId"];
-$qry="select Candidates.id,Candidates.firstname,Candidates.lastname,Candidates.user_id as userid, COALESCE(x.cnt,0) as votecount
+$qry="select Candidates.id,Candidates.firstname,Candidates.lastname,Candidates.user_id as userid, Candidates.description, COALESCE(x.cnt,0) as votecount
 from Candidates
 left outer join (SELECT candidate_id, count(*) cnt from Votes GROUP by candidate_id) x on Candidates.id=x.candidate_id
 where Candidates.election_id='$electionId'";
